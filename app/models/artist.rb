bad999cceb7377d192cbfcc48a3947c9ef12a1e3
Kirsten Keels <keelsk@gmail.com>
2020-03-21 14:26:22 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.all.size
+    self.songs.size
   end
 
   def genre_count
