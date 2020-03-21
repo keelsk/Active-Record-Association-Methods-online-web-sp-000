d4b154593ef0c065c795da2aea115b96ab248e6f
Kirsten Keels <keelsk@gmail.com>
2020-03-21 14:31:07 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,9 +15,9 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     artist_names = []
-    binding.pry
     self.artists.each do |artist| 
       artist_names << artists.name
     end
+    binding.pry
   end
 end
